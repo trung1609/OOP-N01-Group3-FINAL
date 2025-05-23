0e093b1ddaa10c598388e0033750c92382d842e1
NguyenTuongHung <151506436+NguyenTuongHung@users.noreply.github.com>
2025-05-23 14:30:22 +0700

    update

--- src/main/java/entity/Student.java
+++ src/main/java/entity/Student.java
@@ -1,4 +1,106 @@
-package entity;
+package com.example.qlsv_oop_group3.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
+
+@Entity
+@Table(name = "student")
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true)
+    private String studentCode;
+    private String fullName;
+    private String email;
+    private String address;
+    private LocalDate dateOfBirth;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "student")
+    private List<Grade> grades;
+
+    // Default constructor
+    public Student() {
+    }
+
+    // All args constructor
+    public Student(long id, String studentCode, String fullName, String email,
+                   String address, LocalDate dateOfBirth, List<Grade> grades) {
+        this.id = id;
+        this.studentCode = studentCode;
+        this.fullName = fullName;
+        this.email = email;
+        this.address = address;
+        this.dateOfBirth = dateOfBirth;
+        this.grades = grades;
+    }
+
+    // Getters and Setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getStudentCode() {
+        return studentCode;
+    }
+
+    public void setStudentCode(String studentCode) {
+        this.studentCode = studentCode;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public List<Grade> getGrades() {
+        return grades;
+    }
+
+    public void setGrades(List<Grade> grades) {
+        this.grades = grades;
+    }
+
+    public int getAge() {
+        if (dateOfBirth == null) {
+            return 0;
+        }
+        return Period.between(dateOfBirth, LocalDate.now()).getYears();
+    }
 }
--- src/main/java/repo/GradeRepo.java
+++ src/main/java/repo/GradeRepo.java
@@ -1,4 +1,36 @@
-package repo;
+package com.example.qlsv_oop_group3.repo;
 
-public class GradeRepo {
+import com.example.qlsv_oop_group3.entity.Grade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface GradeRepo extends JpaRepository<Grade, Long> {
+    // Lấy danh sách điểm theo id sinh viên
+    List<Grade> findByStudentId(Long studentId);
+
+    // Lấy danh sách điểm theo id môn học
+    List<Grade> findByCourseId(Long courseId);
+
+    // Lấy danh sách điểm theo học kỳ
+    List<Grade> findBySemester(String semester);
+
+
+    // Tìm điểm của một sinh viên cho một môn học cụ thể
+    @Query("Select g from Grade g where g.student.id = :studentId and g.course.id = :courseId")
+    Grade findByStudentIdAndCourseId(@Param("studentId") Long studentId,@Param("courseId") Long courseId);
+
+    // Lấy danh sách điểm của sinh viên theo học kỳ
+    List<Grade> findByStudentIdAndSemester(Long studentId, String semester);
+
+    // Tính GPA học kỳ của sinh viên
+    @Query("SELECT SUM(g.score * g.course.credit) / SUM(g.course.credit) " +
+            "FROM Grade g " +
+            "WHERE g.student.id = :studentId AND g.semester = :semester")
+    Double calculateSemesterGPA(@Param("studentId") Long studentId,
+                                @Param("semester") String semester);
 }
